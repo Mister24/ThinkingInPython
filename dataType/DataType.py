5ea039022a08a6544ac4db3e5e2056e7a24fbb35
Mister24 <motivated24@hotmail.com>
2018-04-01 18:58:26 +0800

    data type

--- dataType/DataType.py
+++ dataType/DataType.py
@@ -1,7 +1,29 @@
-count = 100
-miles = 100.0
-name = "fayuan"
+'''
+基本数据类型
+1.Number（数字）
+2.String（字符串）
+3.List（列表）
+4.Tuple（元组）
+5.Sets（集合）
+6.Dictionary（字典）
+'''
 
+
+#整形变量
+count = 100
+#浮点型变量
+miles = 100.0
+#字符串
+name = "fayuan"
 print(count)
 print(miles)
 print(name)
+
+#连续多个变量赋值
+a, b, c, d = 20, 5.5, True, 4 + 3j
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(isinstance(a, int))
+
